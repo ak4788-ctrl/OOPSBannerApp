b0590b299bccf9edccd04b3bb6f45a4d0ad4fae6
ABISHITH REDDY K <abishithreddy@ABISHITHs-MacBook-Air-683.local>
2026-03-03 09:53:37 +0530

    UC6 - OOPS banner using static functions

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Array declaration + initialization together
+        String[] banner = {
+
+            String.join("  ", " ***** ", " ***** ", "****** ", " ***** "),
+            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("  ", "*     *", "*     *", "*     * ", "*"),
+            String.join("  ", "*     *", "*     *", "****** ", " ***** "),
+            String.join("  ", "*     *", "*     *", "*      ", "      *"),
+            String.join("  ", "*     *", "*     *", "*      ", "*     *"),
+            String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
+
+        };
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
